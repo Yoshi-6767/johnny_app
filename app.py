a230e5e9fab35aa0ec5fc080a9636e7e50c4d029
Yoshi-6767 <vova2006lobanov@gmail.com>
2026-09-20 02:31:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -236,4 +236,5 @@ def progress_page():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
